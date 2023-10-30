a93d384284868708989038a4b0fdb8274c792b75
kobachajchir <kobachajchir2013@gmail.com>
2023-10-30 12:02:40 -0300

    commit navbar

--- frontend/src/components/NavbarComponent.tsx
+++ frontend/src/components/NavbarComponent.tsx
@@ -1,4 +1,3 @@
-import { Button, Container, Dropdown, Image, Navbar } from "react-bootstrap";
 import React, { useContext, useState } from "react"; // Add useState
 import { Link } from "react-router-dom";
 import "./styles/NavbarComponent.css";
