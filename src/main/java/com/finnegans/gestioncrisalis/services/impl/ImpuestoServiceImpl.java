1bc036b159daddf08e173d8a643a79f0f5d7ba43
matiStewartDev <stewartmatias@gmail.com>
2023-11-01 11:51:27 -0300

    arreglando detalles y mmodificacion de la documentacion(nueva version 1.2)

--- src/main/java/com/finnegans/gestioncrisalis/services/impl/ImpuestoServiceImpl.java
+++ src/main/java/com/finnegans/gestioncrisalis/services/impl/ImpuestoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.finnegans.gestioncrisalis.services.impl;
 
 import com.finnegans.gestioncrisalis.dtos.ImpuestoDTO;
+import com.finnegans.gestioncrisalis.exceptions.custom.DataIntegrityException;
 import com.finnegans.gestioncrisalis.exceptions.custom.ResourceNotFound;
 import com.finnegans.gestioncrisalis.models.Impuesto;
 import com.finnegans.gestioncrisalis.repositories.ImpuestoRepository;
@@ -22,14 +23,15 @@ public class ImpuestoServiceImpl implements ImpuestoService {
 
     @Override
     public Impuesto save(ImpuestoDTO impuestoDTO) {
-        Optional<Impuesto> impuestoExist = impuestoRepository.findByNombre(impuestoDTO.getNombreDTO());
+        Optional<Impuesto> impuestoExist = impuestoRepository.findByNombre(impuestoDTO.getNombreDTO().toUpperCase());
 
         if(impuestoExist.isPresent()){
-            throw new RuntimeException();
+            throw new DataIntegrityException("Este impuesto ya se encuentra creado.");
         }
 
+
         Impuesto impuesto = new Impuesto();
-        impuesto.setNombre(impuestoDTO.getNombreDTO());
+        impuesto.setNombre(impuestoDTO.getNombreDTO().toUpperCase());
         impuesto.setPorcentaje(impuestoDTO.getPorcentajeDTO());
 
         return this.impuestoRepository.save(impuesto);
@@ -51,15 +53,14 @@ public class ImpuestoServiceImpl implements ImpuestoService {
         Impuesto impuesto = this.impuestoRepository.findById(id)
                 .orElseThrow(()-> new ResourceNotFound("Impuesto con ID:" + id + " no encotrado"));
         if (StringUtils.isNotBlank(impuestoDTO.getNombreDTO())){
-            impuesto.setNombre(impuestoDTO.getNombreDTO());
+            impuesto.setNombre(impuestoDTO.getNombreDTO().toUpperCase());
         }
 
         if (impuestoDTO.getPorcentajeDTO() != null){
             impuesto.setPorcentaje(impuestoDTO.getPorcentajeDTO());
         }
 
-        Impuesto impuestoSave = this.impuestoRepository.save(impuesto);
-        return impuestoSave;
+        return this.impuestoRepository.save(impuesto);
     }
 
     @Override
